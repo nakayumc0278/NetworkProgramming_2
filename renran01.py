6771f6e8ccf947dc2bcb0860c988c76d17c0b090
chihiro0325 <nintr_0513@yahoo.co.jp>
2020-11-17 11:32:16 +0900

    Python Program

--- renran01.py
+++ renran01.py
@@ -1,5 +1,9 @@
+#renran01.py
+#coding: utf-8
+
 import random as rd
 
+#アルファベット配置
 alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "h", "j", "k", "l", "m", \
     "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
@@ -15,4 +19,3 @@ while True:
     else:
         print("OK")
         break
-    
