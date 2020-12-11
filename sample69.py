28c38789f9c5bfcefd2e2e25b47620b0d137811b
Chihiro <40320338+nakayumc0278@users.noreply.github.com>
2020-12-11 14:25:37 +0900

    Python Program

---
+++ sample69.py
@@ -0,0 +1,17 @@
+# sample69.py
+# coding= utf-8
+
+try:
+    f = open("sample69.txt","r",encoding='utf-8')
+
+except FileNotFoundError:
+    print("ファイルがオープンできない")
+
+else:
+    lines = f.readline()
+    for line in lines:
+        print(line,end="")
+    f.close()
+
+finally:
+    print("処理終了")
