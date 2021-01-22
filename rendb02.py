3cf05118bf237b95fcbc091bbdc9edbfd7342cc3
Chihiro <40320338+nakayumc0278@users.noreply.github.com>
2021-01-22 14:54:10 +0900

    add: Database

---
+++ rendb02.py
@@ -0,0 +1,15 @@
+# rendb02.py
+# coding: utf-8
+
+import sqlite3 as sql
+
+conn = sql.connect("sample.db")
+
+c = conn.cursor()
+
+itr = c.execute("select * from products order by price desc")
+
+for row in itr:
+    print(row)
+
+conn.close()
